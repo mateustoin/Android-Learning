28881af2f73c53911365410e46fba44c5bf0959f
Mateus Antonio <mateustoin@gmail.com>
2026-06-17 14:30:28 -0300

    Update minSdk version from 36 to 33

--- StudyApp/app/build.gradle.kts
+++ StudyApp/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.studyapp"
-        minSdk = 36
+        minSdk = 33
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
